c231f0d881003d588e345f07d5e740e95de61523
Jack Phillips <jackphillipsspb@gmail.com>
2020-03-03 19:19:24 +0300

    Change db on MySQL

--- mysite/settings.py
+++ mysite/settings.py
@@ -76,8 +76,11 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'JackPhillips$iCT',
+        'USER': 'JackPhillips',
+        'PASSWORD': 's%fGGrEP!zIvkIoZNaZ&G7Uu',
+        'HOST': 'JackPhillips.mysql.pythonanywhere-services.com',
     }
 }
 
